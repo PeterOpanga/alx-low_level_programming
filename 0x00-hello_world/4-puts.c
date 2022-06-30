a678ca0c7754d44f48443707aae22e9f8835102f
PeterOpanga <peteropanga@gmail.com>
2022-06-30 12:41:01 -0700

    ALX Low Level Programming 0X00 - Hello, World Project by @PeterOpanga

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,20 +1,15 @@
+
 #include <stdio.h>
 
-
 /**
-
- * main - prints the phrase "Programming is like building a multilingual puzzle
-
- * Return: 0 if exited properly, non-zero otherwise
-
- */
-
+* main - print the string in the put function
+*
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
+*/
 int main(void)
-
 {
-
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
-
 }
