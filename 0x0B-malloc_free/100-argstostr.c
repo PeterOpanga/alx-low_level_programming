1fea5e2da129358f445a50e1cc3377200ebbcd5a
PeterOpanga <peteropanga@gmail.com>
2022-07-26 11:41:34 -0700

    ALX LLP - 0x0B - Malloc, Free Project by @PeterOpanga

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -23,7 +23,8 @@ char *argstostr(int ac, char **av)
 		size++;
 	}
 	size++;
-/* allocate memory for total number of chars and
+/**
+ * allocate memory for total number of chars and
  * new line for each word
  */
 	strDup = malloc(sizeof(char) * size);
