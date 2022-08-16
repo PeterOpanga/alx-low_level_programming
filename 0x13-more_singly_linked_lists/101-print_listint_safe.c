64d588f39a41bb5a6b964afac04d27265d412c29
PeterOpanga <peteropanga@gmail.com>
2022-08-16 10:47:33 -0700

    ALX LLP - 0x13 - More Singly Linked Lists Project by @PeterOpanga

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,26 +4,25 @@
  * print_listint_safe - prints a linked list, safely
  * @head: list of type listint_t to print
  *
- * Return: number of nodes in the list
+ * Return: i
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t num = 0;
-	long int diff;
+	size_t i = 0;
+	const listint_t *temp, *node;
 
-	while (head)
+	node = head;
+	while (node != NULL)
 	{
-		diff = head - head->next;
-		num++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (diff > 0)
-			head = head->next;
-		else
+		printf("[%p] %d\n", (void *)node, node->n);
+		temp = node;
+		node = node->next;
+		i++;
+		if (temp <= node)
 		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			printf("-> [%p] %d\n", (void *)node, node->n);
 			break;
 		}
 	}
-
-	return (num);
+	return (i);
 }
